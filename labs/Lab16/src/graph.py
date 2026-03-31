1f3ed5f38304fb0cc7caa9f6ac2e925ed1a31a3b
jcd1141 <joshd3887@gmail.com>
2026-03-31 18:23:53 +0000

    Lab 16: Build the graph

--- labs/Lab16/src/graph.py
+++ labs/Lab16/src/graph.py
@@ -72,6 +72,19 @@ def build_lab_graph():
 
     # TODO: Add all six nodes (A through F)
     # TODO: Add all six edges
+    g.add_node("A")
+    g.add_node("B")
+    g.add_node("C")
+    g.add_node("D")
+    g.add_node("E")
+    g.add_node("F")
+
+    g.add_edge("A", "B")
+    g.add_edge("A", "C")
+    g.add_edge("B", "D")
+    g.add_edge("C", "E")
+    g.add_edge("D", "F")
+    g.add_edge("E", "F")
 
     return g
 
